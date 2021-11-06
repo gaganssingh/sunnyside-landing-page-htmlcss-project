a7867a9c641cb0b49bbf7641b11c39ae78b55663
Gagan Singh <gagansingh.dev@outlook.com>
2021-11-06 15:09:40 -0400

    Styled the Services section

--- js/app.js
+++ js/app.js
@@ -8,10 +8,11 @@ const obs = new IntersectionObserver(
     const ent = entries[0];
 
     if (!ent.isIntersecting) {
-      primaryHeader.style.backgroundColor = "hsl(212, 27%, 19%)";
       primaryHeader.style.backgroundColor = "hsl(200, 100%, 62%)";
+      primaryHeader.style.boxShadow = "0px 2px 20px rgba(0, 0, 0, 0.5)";
     } else if (ent.isIntersecting) {
       primaryHeader.style.backgroundColor = "transparent";
+      primaryHeader.style.boxShadow = "none";
     }
   },
   {
